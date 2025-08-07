e5e1a15c21f3333483b49af9756fdc24f386cee8
skorjen <es@cstmarket.ru>
2025-08-07 19:23:28 +0300

    flake testing

--- routes.py
+++ routes.py
@@ -2,16 +2,8 @@ from typing import List
 
 from flask_openapi3 import APIBlueprint, Tag
 
-from models import Client
 from repository import ClientRepository, CPRepository, ParkingRepository
-from shemas import (
-    ClientIdShema,
-    ClientShema,
-    ClientShemaOUT,
-    CPShema,
-    CPShemaDel,
-    ParkingShema,
-)
+from shemas import ClientIdShema, ClientShema, CPShema, CPShemaDel, ParkingShema
 
 index_bp = APIBlueprint("index", __name__)
 clients_bp = APIBlueprint("clients", __name__, url_prefix="/api/clients")
@@ -34,8 +26,7 @@ def main():
 @clients_bp.get("/", summary="Список всех клиентов", tags=[cli_tag])
 def get_clients() -> List[dict]:
     """Получение списка клиентов"""
-    clients = ClientRepository.get_all_clients_db()
-    return clients
+    return ClientRepository.get_all_clients_db()
 
 
 @clients_bp.post("/", summary="Добавить клиента", tags=[cli_tag])
